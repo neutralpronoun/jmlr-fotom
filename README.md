d91669f74b65f9f9788513a4a3ab071e03eeb8a3
neutralpronoun <alexowendavies@gmail.com>
2024-01-11 21:58:20 +0000

    adding readme

--- README.md
+++ README.md
@@ -1,9 +1,68 @@
 # FoToM
 
-Repository for our (ongoing) work on using adversarial graph constrastive learning to produce domain-agnostic graph representations.
+Repository for the paper **A Foundation Graph Model** in submission at ICML as of 01/02/24.
 
+### Abstract:
 
-An interactive version of the embedding space, through PCA, can be explored in-browser through `bokeh-embedding-dashboard.html`
+The principal benefit of unsupervised graph representation learning is that a pre-trained model can be fine-tuned where data or labels are scarce.
+Current methods maintain consistent node and edge attributes across the pretraining and target datasets. 
+This precludes transfer to other domains.
+A model capable of positive transfer on an arbitrary task and domain represents the first foundation graph model.
+
+![Domain transfer diagram](https://github.com/neutralpronoun/general-gcl/blob/main/figures/domains.drawio.png)
+
+In this work we use adversarial contrastive learning to present FOTOM, the first foundation graph model. 
+By excluding node and edge attributes we are able to pre-train over varied graph domains.
+We demonstrate positive transfer on evaluation datasets from multiple domains compared to a non-pretrained model. 
+This includes domains not present in pre-training data. 
+Contrary to other research, pre-training on a dataset with the target domain excluded leads us to better performance than pre-training on a dataset from only the target domain. 
+This includes when node labels are used in evaluation, where performance is consistently superior to single-domain or non-pre-trained models.
+
+## Code Usage
+
+The environmental setup is fairly minimal.
+The broad requirements are standard for graph deep-learning:
+
+ - pytorch-geometric
+ - pytorch (shocking I know)
+ - numpy, scipy, matplotlib, pandas, etc.
+ - ogb (open graph benchmark)
+
+Apologies for this being a rather brief description. 
+Pytorch-geometric is still a little volatile, and often doesn't play well with others depending on your hardware, so we leave it up to the user to get out of versioning hell.
+
+### Training
+`train.py --args` can be used to train new FoToM models.
+
+Most arguments have descriptions accessible via `--help`.
+Arguments specific to FoToM are as follows:
+
+ - `-f --node-features`       whether to use node labels during evaluation
+ - `-c --no-molecules`        whether to exclude molecules from training data
+ - `-s --no-socials`          whether to exclude non-molecules from training data
+ - `-rn --randon-node-views`  whether to switch from adversarial augmentations to random node dropping
+ - `-re --randon-edge-views`  whether to switch from adversarial augmentations to random edge dropping
+ - `--dropped`                if using random edge/node dropping, what proportion to drop
+
+### Transfer
+
+`transfer.py --args` can be used to fine-tune FoToM models.
+Please not that it does this on all of our validation datasets.
+
+ - `--num` the maximum number of samples to include in the test sets (doubled for validation)
+ - `--checkpoint` the model checkpoint to fine-tune. We include the following:
+ - - `all-100.pt` a model trained for 100 epochs on all the training data
+ - - `chem-100.pt` a model trained for 100 epochs on only molecules
+ - - `social-100.pt` a model trained for 100 epochs on on non-molecules
+ - `untrained` as a checkpoint will not load a checkpoint, instead training a model from scratch
+
+`linear_transfer.py` functions in essentially the same way, except that it uses a linear model in place of fine-tuning a FoToM model.
+
+#### Further code
+
+Dataset code can be found under `/datasets/`.
+Loaders can be found here under `loaders.py`, and other datasets have their own respective processing files.
+`from_ogb_dataset.py` converts an OGB dataset into a standard pytorch-geometric dataset.
 
 
 
--- transfer.py
+++ transfer.py
@@ -138,7 +138,6 @@ def evaluate_model(model, test_loader, score_fn, out_fn, loss_fn, task):
 
 def fine_tune(model, checkpoint_path, val_loader, test_loader, name = "blank", n_epochs = 50):
     # At the moment this is rigid to single-value predictions
-
     device = "cuda" if torch.cuda.is_available() else "cpu"
     task = get_task_type(val_loader, name)
 
@@ -300,31 +299,21 @@ def run(args):
 
         best_pretrain_score = 1.e07
         best_untrain_score = 1.e07
-        best_default_score = 1.e07
 
         pretrain_val = np.zeros((n_repeats, num_epochs))
         untrain_val = np.zeros((n_repeats, num_epochs))
-        default_val = np.zeros((n_repeats, num_epochs))
-        pretrain_scores, untrain_scores, default_scores = [], [], []
+        pretrain_scores, untrain_scores = [], []
 
         pbar = tqdm(range(n_repeats))
         for n in pbar:
-            model = FeaturedTransferModel(
+            model = TransferModel(
                 Encoder(emb_dim=args.emb_dim, num_gc_layers=args.num_gc_layers, drop_ratio=args.drop_ratio, pooling_type=args.pooling_type),
-                proj_hidden_dim=args.emb_dim, output_dim=1, features=evaluation_node_features,
-                node_feature_dim=atom_feature_dims, edge_feature_dim=bond_feature_dims).to(device)
-
-            pretrain_train_losses, pretrain_val_losses, pretrain_val_score, pretrain_best_epoch, pretrain_best_val_loss = fine_tune(model,
-                                                                                                                                    checkpoint_path,
-                                                                                                                                    val_loader,
-                                                                                                                                    test_loader,
-                                                                                                                                    name = name,
-                                                                                                                                    n_epochs=num_epochs)
-            model = FeaturedTransferModel(
+                proj_hidden_dim=args.emb_dim, output_dim=1, features=evaluation_node_features).to(device)
+            pretrain_train_losses, pretrain_val_losses, pretrain_val_score, pretrain_best_epoch, pretrain_best_val_loss = fine_tune(model, checkpoint_path, val_loader, test_loader, name = name, n_epochs=num_epochs)
+            model = TransferModel(
                 Encoder(emb_dim=args.emb_dim, num_gc_layers=args.num_gc_layers, drop_ratio=args.drop_ratio,
                         pooling_type=args.pooling_type),
-                proj_hidden_dim=args.emb_dim, output_dim=1, features=evaluation_node_features,
-                node_feature_dim=atom_feature_dims, edge_feature_dim=bond_feature_dims).to(device)
+                proj_hidden_dim=args.emb_dim, output_dim=1, features=evaluation_node_features).to(device)
             untrain_train_losses, untrain_val_losses, untrain_val_score, untrain_best_epoch,untrain_best_val_loss = fine_tune(model,
                                                                                                                               "untrained",
                                                                                                                               val_loader,
@@ -332,76 +321,48 @@ def run(args):
                                                                                                                               name = name,
                                                                                                                               n_epochs=num_epochs)
 
-            model = TransferModel(
-                Encoder(emb_dim=args.emb_dim, num_gc_layers=args.num_gc_layers, drop_ratio=args.drop_ratio,
-                        pooling_type=args.pooling_type),
-                proj_hidden_dim=args.emb_dim, output_dim=1, features=evaluation_node_features).to(device)
-            default_train_losses, default_val_losses, default_val_score, default_best_epoch, default_best_val_loss = fine_tune(model,
-                                                                                                                              checkpoint_path,
-                                                                                                                              val_loader,
-                                                                                                                              test_loader,
-                                                                                                                              name = name,
-                                                                                                                              n_epochs=num_epochs)
-
             pretrain_val[n, :] = pretrain_val_losses
             untrain_val[n, :] = untrain_val_losses
-            default_val[n, :] = default_val_losses
 
-            scores = "Pretrain:" + str(pretrain_val_score)[:5]  + "  Untrain:" + str(untrain_val_score)[:5] + "default:  " +  str(default_val_score)[:5]
+            scores = "Pretrain:" + str(pretrain_val_score)[:5]  + "  Untrain:" + str(untrain_val_score)[:5]
             pbar.set_description(scores)
 
             pretrain_scores.append(pretrain_val_score)
             untrain_scores.append(untrain_val_score)
-            default_scores.append(default_val_score)
 
             if pretrain_val_score <= best_pretrain_score:
                 best_pretrain_score = pretrain_val_score
             if untrain_val_score <= best_untrain_score:
                 best_untrain_score = untrain_val_score
-            if default_val_score <= best_default_score:
-                best_default_score = default_val_score
 
         untrain_val_score = str(best_untrain_score)[:6]
         pretrain_val_score = str(best_pretrain_score)[:6]
-        default_val_score = str(best_default_score)[:6]
 
         pretrain_val_loss_mean = np.mean(pretrain_val, axis=0)
         untrain_val_loss_mean = np.mean(untrain_val, axis=0)
-        default_val_loss_mean = np.mean(default_val, axis=0)
 
         pretrain_val_loss_max = np.max(pretrain_val, axis=0)
         untrain_val_loss_max = np.max(untrain_val, axis=0)
-        default_val_loss_max = np.max(default_val, axis=0)
 
         pretrain_val_loss_min = np.min(pretrain_val, axis=0)
         untrain_val_loss_min = np.min(untrain_val, axis=0)
-        default_val_loss_min = np.min(default_val, axis=0)
 
         pretrain_mean_score = str(np.mean(pretrain_scores))[:5]
         untrain_mean_score = str(np.mean(untrain_scores))[:5]
-        default_mean_score = str(np.mean(default_scores))[:5]
 
         pretrain_dev_score = str(np.std(pretrain_scores))[:5]
         untrain_dev_score = str(np.std(untrain_scores))[:5]
-        default_dev_score = str(np.std(default_scores))[:5]
 
         fig, ax = plt.subplots(figsize=(6, 4))
 
+        wandb.log({f"{name}/model-mean": float(pretrain_mean_score),
+                   f"{name}/model-dev": float(pretrain_dev_score),
+                   f"{name}/model-best": float(pretrain_val_score)})
 
+        wandb.log({f"{name}/untrained-mean": float(untrain_mean_score),
+                   f"{name}/untrained-dev": float(untrain_dev_score),
+                   f"{name}/untrained-best": float(untrain_val_score)})
 
-
-
-        ax.fill_between(np.linspace(start = 0, stop = num_epochs, num=default_val_loss_mean.shape[0]),
-                default_val_loss_max,
-                default_val_loss_min,
-                alpha = 0.5,
-                color = "orange")
-
-
-        ax.plot(np.linspace(start = 0, stop = num_epochs, num=default_val_loss_mean.shape[0]),
-                default_val_losses,
-                label=f"Default, Score: {default_mean_score} +/- {default_dev_score},  Best: {default_val_score}",
-                c = "orange")
 
         ax.fill_between(np.linspace(start = 0, stop = num_epochs, num=untrain_val_loss_mean.shape[0]),
                 untrain_val_loss_max,
@@ -424,7 +385,7 @@ def run(args):
 
         ax.plot(np.linspace(start = 0, stop = num_epochs, num=pretrain_val_loss_mean.shape[0]),
                 pretrain_val_losses,
-                label=f"Input Head ({model_name}), Score: {pretrain_mean_score} +/- {pretrain_dev_score},  Best: {pretrain_val_score}",
+                label=f"Pre-Trained ({model_name}), Score: {pretrain_mean_score} +/- {pretrain_dev_score},  Best: {pretrain_val_score}",
                 c = "green")
 
         ax.legend(loc = "upper right", shadow=True)
@@ -473,9 +434,6 @@ def arg_parse():
 
     parser.add_argument('--num', type=int, default=1000,
                         help='Number of points included in each dataset')
-
-    parser.add_argument('--redo_views', type=bool, default=False,
-                        help='Whether to re-vis views')
 
     parser.add_argument('--checkpoint', type=str, default="latest", help='Either the name of the trained model checkpoint in ./outputs/, or latest for the most recent trained model in ./wandb/latest-run/files')
 
